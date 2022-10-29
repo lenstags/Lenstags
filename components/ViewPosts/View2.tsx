30fc65e08d53d3e65d7edf7f44b3874ea4a4c8f5
Esau Guerra <eguerra@mitsoftware.com>
2022-10-29 15:00:25 -0400

    Post section added

--- components/ViewPosts/View2.tsx
+++ components/ViewPosts/View2.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import {Input} from '../UI/Search'
+import PostType from '../UI/PostTypes'
 
 const View2 = () => {
   return (
-    <div className='w-full pt-10 pl-5'>
+    <div className='w-full pt-10 pl-5 pb-40'>
       <div className='w-full flex'>
           <div className='flex gap-8 border-black border w-1/2'>
             <div>MY LISTS</div>
@@ -18,7 +19,11 @@ const View2 = () => {
                   />
           </div>
       </div>
-      <div></div>
+      <div>
+
+        <PostType/>
+
+      </div>
     </div>
   )
 }
